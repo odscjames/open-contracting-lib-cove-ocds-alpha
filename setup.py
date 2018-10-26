00b4d60bcb4bfbef50068f71fcdaf970381d8e74
James Baster <james.baster@opendataservices.coop>
2018-10-26 12:00:06 +0100

    START

---
+++ setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='libcoveocds',
+    version='0.0.0',
+    author='Open Contracting Partnership',
+    author_email='data@open-contracting.org',
+    url='https://github.com/xxxxxxxxxxxxx/xxxxxx',
+    description='A data review library',
+    license='BSD',
+    packages=find_packages(),
+    long_description='A data review library',
+    install_requires=[
+        'jsonref',
+        'jsonschema',
+        'CommonMark',
+        'bleach',
+        'requests',
+    ],
+    entry_points='''[console_scripts]
+libcoveocds = libcoveocds.cli.__main__:main''',
+)
